657073a98be85d57a8ce6ff11563c5921856a76e
Virginie Trubiano & Bojana Skarich <student@epicodus.com>
2015-04-23 15:52:32 -0700

    Add new postStillRelevant method to filter post time.

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -1,4 +1,29 @@
 hackerNews.controller('PostsCtrl', function PostsCtrl($scope, PostsFactory) {
   $scope.posts = PostsFactory.posts;
   $scope.PostsFactory = PostsFactory;
+
+  // var Timer = setInterval(function () {$scope.postStatus()}, 2000);
+
+  // $scope.deletePost = function(post) {
+  //     var index = this.posts.indexOf(post);
+  //     this.posts.splice(index, 1);
+  // };
+
+  $scope.postIsStillRelevant = function(post) {
+      console.log("hello");
+      var timeElapsed = new Date() - post.date;
+      return timeElapsed < 10000;
+  };
+
+  // $scope.postStatus = function() {
+  //     for (var i = 0; i < this.posts.length; i++) {
+  //         if (this.posts[i].timeLife !== 0) {
+  //             this.posts[i].timeLife -= 1;
+  //         } else {
+  //             var post = this.posts[i]
+  //             this.deletePost(post);
+  //         }
+  //     }
+  // };
+
 });
--- services/PostsFactory.js
+++ services/PostsFactory.js
@@ -3,7 +3,7 @@ hackerNews.factory('PostsFactory', function PostsFactory() {
     factory.posts = [];
 
     factory.addPost = function() {
-        factory.posts.push({ title: factory.postTitle, link: factory.postLink, ranking: 0, id: factory.posts.length + 1, comments: [], date: new Date() });
+        factory.posts.push({ title: factory.postTitle, link: factory.postLink, ranking: 0, id: factory.posts.length + 1, comments: [], date: new Date(), timeLife: 3 });
         factory.postTitle = null;
         factory.postLink = null;
     };
@@ -20,6 +20,25 @@ hackerNews.factory('PostsFactory', function PostsFactory() {
     factory.raiseRank = function(item) {
         item.ranking += 1;
     };
+
+
+    // var Timer = setInterval(function () {factory.postStatus()}, 2000);
+    //
+    // factory.postStatus = function() {
+    //     console.log('running postStatus()...')
+    //     console.log(this.posts);
+    //     for (var i = 0; i < this.posts.length; i++) {
+    //         if (this.posts[i].timeLife !== 0) {
+    //             console.log('this post is not dead yet!');
+    //             this.posts[i].timeLife -= 1;
+    //         } else {
+    //             console.log('well, all good things must come to an end');
+    //             this.posts.splice(i, 1);
+    //             console.log(this.posts);
+    //         }
+    //     }
+    // };
+
 
     return factory;
 });
